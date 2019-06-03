e5f8af4313152ecd371f5fd4ff911e731798b05e
you-mine <yu-minei@occ.co.jp>
2019-06-03 16:13:10 +0900

    RESTSERVICE修正

--- LivetApp1/LivetApp1/Services/RestService.cs
+++ LivetApp1/LivetApp1/Services/RestService.cs
@@ -15,15 +15,11 @@ namespace LivetApp1.Services
         private HttpClient Client;
         private string BaseUrl;
 
-
-
-
         public RestService()
         {
             this.Client = new HttpClient();
             this.BaseUrl = "https://localhost:5001";
         }
-
 
         #region ログイン処理
         public async Task<User> LogonAsync(User user)
@@ -105,6 +101,21 @@ namespace LivetApp1.Services
                 // TODO
                 System.Diagnostics.Debug.WriteLine("Exception in RestService.LogonAsync: " + e);
             }
+
+            var a =
+                responseCards
+                            .Where(x => x.PostDate < DateTime.Today)
+                            .GroupBy(x => new { x.To.Id })
+                            .Select(
+                                        x => new {
+                                            x.Key.Id
+                                                ,
+                                            名前 = x.Select(y => y.To.Name).ToArray()[0]
+                                                ,
+                                            総数 = x.Select(y => y.ThanksCount).Sum()
+                                        }
+                                    ).ToList();
+
             return responseCards;
         }
         #endregion
@@ -180,8 +191,6 @@ namespace LivetApp1.Services
             return "failed";
         }
         #endregion
-
-
 
     }
 }
