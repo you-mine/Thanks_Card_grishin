ec88c4f9e55b50b5dbcb7a173685a1d0eb51039f
mirei08 <mirei@occ.co.jp>
2019-06-04 17:26:34 +0900

    com

---
+++ LivetApp1/LivetApp1/Models/Ranking.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Livet;
+
+namespace LivetApp1.Models
+{
+    public class Ranking : NotificationObject
+    {
+        #region Rank Property
+        private int _Rank;
+
+        public int Rank
+        {
+            get
+            { return _Rank; }
+            set
+            { 
+                if (_Rank == value)
+                    return;
+                _Rank = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Name Property
+
+        private string _Name;
+
+        public string Name
+        {
+            get
+            { return _Name; }
+            set
+            { 
+                if (_Name == value)
+                    return;
+                _Name = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        #endregion
+
+        #region DepatmentName Property
+
+        private string _DepartmentName;
+
+        public string DepartmentName
+        {
+            get
+            { return _DepartmentName; }
+            set
+            { 
+                if (_DepartmentName == value)
+                    return;
+                _DepartmentName = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        #endregion
+
+        #region ThanksCount Property
+
+        private int _ThanksCount;
+
+        public int ThanksCount
+        {
+            get
+            { return _ThanksCount; }
+            set
+            { 
+                if (_ThanksCount == value)
+                    return;
+                _ThanksCount = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        #endregion
+
+
+    }
+}
